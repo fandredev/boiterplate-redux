0b796a2b627193650a2bf974a52e908546b1f6ee
felipe <profissionalf.andre@gmail.com>
2024-10-01 09:08:50 -0300

    refactor: Update BalanceDisplay component to dynamically display account balance
    - Import the `useSelector` hook from `react-redux` to access the account state
    - Use the `balance` property from the account state to dynamically format and display the account balance
    - Remove hard-coded value and replace it with the `balance` variable in the `formatCurrency` function

--- src/features/accounts/BalanceDisplay.tsx
+++ src/features/accounts/BalanceDisplay.tsx
@@ -1,10 +1,15 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+
 function formatCurrency(value: number) {
-  return new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'USD',
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "USD",
   }).format(value);
 }
 
 export default function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+  const { balance } = useSelector((state: RootState) => state.account);
+
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
