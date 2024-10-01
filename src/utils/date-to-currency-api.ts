6fdff13dc3ebaf73deefa8603dfe941e165b692b
felipe <profissionalf.andre@gmail.com>
2024-10-01 09:06:55 -0300

    feat: Add date-to-currency-api utility function
    This commit adds a new utility function called date-to-currency-api.ts. This function formats the current date and time in the "pt-BR" locale and returns it as a string without commas and leading whitespace.

---
+++ src/utils/date-to-currency-api.ts
@@ -0,0 +1,14 @@
+const today = new Date();
+
+export const formattedDate = today
+  .toLocaleString("pt-BR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  })
+  .replace(",", "")
+  .trimStart();
